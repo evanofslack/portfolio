8c949e3a3030e74cc9948298f7992865f683dcf1
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-06-06 16:58:46 -0400

    Added new secondary font
    public-sans

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from './App';
 import 'typeface-mada';
 import 'typeface-karla';
+import 'typeface-public-sans';
+require('typeface-public-sans')
 
 ReactDOM.render(
   <App/>,
