d98fbe4b4d5c9abdbd529c7d914ecdd24cd95897
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-06-20 19:09:00 -0400

    prunned unused dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from './App';
 import 'typeface-mada';
 import 'typeface-karla';
-import 'typeface-public-sans';
 import 'typeface-barlow';
 import "@fontsource/barlow"; // Defaults to weight 400.
 
