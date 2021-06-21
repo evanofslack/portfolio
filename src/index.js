c9bfb52e2c85dfb551b7e89705caf561e254b0cc
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-06-20 20:08:03 -0400

    Revert "prunned unused dependencies"
    This reverts commit d98fbe4b4d5c9abdbd529c7d914ecdd24cd95897.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from './App';
 import 'typeface-mada';
 import 'typeface-karla';
+import 'typeface-public-sans';
 import 'typeface-barlow';
 import "@fontsource/barlow"; // Defaults to weight 400.
 
