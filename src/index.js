e8b8f80a43800572e2bdb25d88d6d9f1fb0e7251
evanofslack <51209817+evanofslack@users.noreply.github.com>
2021-06-20 20:17:07 -0400

    Prune dependencies again

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom";
 import App from './App';
 import 'typeface-mada';
 import 'typeface-karla';
-import 'typeface-public-sans';
 import 'typeface-barlow';
-import "@fontsource/barlow"; // Defaults to weight 400.
+
 
 
 ReactDOM.render(
